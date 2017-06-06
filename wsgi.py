2131822d0d028f226f63f6bbbe0788e203fc0b22
Lindsey Tulloch <onyiny-ang@users.noreply.github.com>
2017-06-06 15:18:12 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'patw',
+        'displayName': 'parks around the world',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
